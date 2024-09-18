d7485acb77adf0e4543c1ae6ee4caa98c3dd580b
hamzatalha77 <hamza_ta7@hotmail.com>
2024-09-18 08:46:32 +0100

    commit

--- app/components/Collections/index.tsx
+++ app/components/Collections/index.tsx
@@ -23,10 +23,10 @@ const Collections = () => {
                 className='absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
               />
 
-              <div className='relative flex w-full flex-col rounded-lg bg-white p-4 text-center'>
+              <div className='relative flex w-full flex-col rounded-lg p-4 text-center backdrop-blur-sm'>
                 <span className='text-gray-500'>womans</span>
                 <span className='text-lg font-bold text-gray-800 lg:text-xl'>
-                  Business Causual
+                  Business
                 </span>
               </div>
             </a>
@@ -45,7 +45,7 @@ const Collections = () => {
                 className='absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
               />
 
-              <div className='relative flex w-full flex-col rounded-lg bg-white p-4 text-center'>
+              <div className='relative flex w-full flex-col rounded-lg p-4 text-center backdrop-blur-sm'>
                 <span className='text-gray-500'>Women</span>
                 <span className='text-lg font-bold text-gray-800 lg:text-xl'>
                   Summer Season
@@ -67,7 +67,7 @@ const Collections = () => {
                 className='absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
               />
 
-              <div className='relative flex w-full flex-col rounded-lg bg-white p-4 text-center'>
+              <div className='relative flex w-full flex-col rounded-lg p-4 text-center backdrop-blur-sm'>
                 <span className='text-gray-500'>Men</span>
                 <span className='text-lg font-bold text-gray-800 lg:text-xl'>
                   Streetwear
@@ -89,7 +89,7 @@ const Collections = () => {
                 className='absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
               />
 
-              <div className='relative flex w-full flex-col rounded-lg bg-white p-4 text-center'>
+              <div className='relative flex w-full flex-col rounded-lg p-4 text-center backdrop-blur-sm'>
                 <span className='text-gray-500'>Women</span>
                 <span className='text-lg font-bold text-gray-800 lg:text-xl'>
                   Sale
