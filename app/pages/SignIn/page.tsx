49e2626f80567a74df21a1ed88a73a30735c2ab0
hamzatalha77 <hamza_ta7@hotmail.com>
2024-09-30 12:14:46 +0100

    commit

--- app/pages/SignIn/page.tsx
+++ app/pages/SignIn/page.tsx
@@ -94,7 +94,6 @@ const SignIn = () => {
                       href='/pages/SignUp'
                       className='text-lg text-second decoration-solid'
                     >
-                      {' 
                       Sign Up
                     </Link>
                   </p>
