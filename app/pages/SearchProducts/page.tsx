c1905ed4d6467b5f54020335b21d2bb679a6da0f
hamzatalha77 <hamza_ta7@hotmail.com>
2024-10-07 10:29:21 +0100

    commit

--- app/pages/SearchProducts/page.tsx
+++ app/pages/SearchProducts/page.tsx
@@ -108,132 +108,137 @@ const SearchProducts = () => {
                       type='button'
                       className='flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500'
                       aria-controls='filter-section-mobile-0'
-                      aria-expanded='false'
+                      aria-expanded={isColorOpen}
+                      onClick={toggleColorSection}
                     >
                       <span className='font-medium text-gray-900'>Color</span>
                       <span className='ml-6 flex items-center'>
-                        <svg
-                          className='h-5 w-5'
-                          viewBox='0 0 20 20'
-                          fill='currentColor'
-                          aria-hidden='true'
-                          data-slot='icon'
-                        >
-                          <path d='M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z' />
-                        </svg>
-
-                        <svg
-                          className='h-5 w-5'
-                          viewBox='0 0 20 20'
-                          fill='currentColor'
-                          aria-hidden='true'
-                          data-slot='icon'
-                        >
-                          <path
-                            fill-rule='evenodd'
-                            d='M4 10a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z'
-                            clip-rule='evenodd'
-                          />
-                        </svg>
+                        {isColorOpen ? (
+                          <svg
+                            className='h-5 w-5'
+                            viewBox='0 0 20 20'
+                            fill='currentColor'
+                            aria-hidden='true'
+                            data-slot='icon'
+                          >
+                            <path
+                              fillRule='evenodd'
+                              d='M4 10a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H4.75A.75.75 0 0 1 4 10Z'
+                              clipRule='evenodd'
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            className='h-5 w-5'
+                            viewBox='0 0 20 20'
+                            fill='currentColor'
+                            aria-hidden='true'
+                            data-slot='icon'
+                          >
+                            <path d='M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z' />
+                          </svg>
+                        )}
                       </span>
                     </button>
                   </h3>
 
-                  <div className='pt-6' id='filter-section-mobile-0'>
-                    <div className='space-y-6'>
-                      <div className='flex items-center'>
-                        <input
-                          id='filter-mobile-color-0'
-                          name='color[]'
-                          value='white'
-                          type='checkbox'
-                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-                        />
-                        <label
-                          htmlFor='filter-mobile-color-0'
-                          className='ml-3 min-w-0 flex-1 text-gray-500'
-                        >
-                          White
-                        </label>
-                      </div>
-                      <div className='flex items-center'>
-                        <input
-                          id='filter-mobile-color-1'
-                          name='color[]'
-                          value='beige'
-                          type='checkbox'
-                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-                        />
-                        <label
-                          htmlFor='filter-mobile-color-1'
-                          className='ml-3 min-w-0 flex-1 text-gray-500'
-                        >
-                          Beige
-                        </label>
-                      </div>
-                      <div className='flex items-center'>
-                        <input
-                          id='filter-mobile-color-2'
-                          name='color[]'
-                          value='blue'
-                          type='checkbox'
-                          checked
-                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-                        />
-                        <label
-                          htmlFor='filter-mobile-color-2'
-                          className='ml-3 min-w-0 flex-1 text-gray-500'
-                        >
-                          Blue
-                        </label>
-                      </div>
-                      <div className='flex items-center'>
-                        <input
-                          id='filter-mobile-color-3'
-                          name='color[]'
-                          value='brown'
-                          type='checkbox'
-                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-                        />
-                        <label
-                          htmlFor='filter-mobile-color-3'
-                          className='ml-3 min-w-0 flex-1 text-gray-500'
-                        >
-                          Brown
-                        </label>
-                      </div>
-                      <div className='flex items-center'>
-                        <input
-                          id='filter-mobile-color-4'
-                          name='color[]'
-                          value='green'
-                          type='checkbox'
-                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-                        />
-                        <label
-                          htmlFor='filter-mobile-color-4'
-                          className='ml-3 min-w-0 flex-1 text-gray-500'
-                        >
-                          Green
-                        </label>
-                      </div>
-                      <div className='flex items-center'>
-                        <input
-                          id='filter-mobile-color-5'
-                          name='color[]'
-                          value='purple'
-                          type='checkbox'
-                          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
-                        />
-                        <label
-                          htmlFor='filter-mobile-color-5'
-                          className='ml-3 min-w-0 flex-1 text-gray-500'
-                        >
-                          Purple
-                        </label>
+                  {isColorOpen && (
+                    <div className='pt-6' id='filter-section-mobile-0'>
+                      <div className='space-y-6'>
+                        <div className='flex items-center'>
+                          <input
+                            id='filter-mobile-color-0'
+                            name='color[]'
+                            value='white'
+                            type='checkbox'
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                          />
+                          <label
+                            htmlFor='filter-mobile-color-0'
+                            className='ml-3 min-w-0 flex-1 text-gray-500'
+                          >
+                            White
+                          </label>
+                        </div>
+                        <div className='flex items-center'>
+                          <input
+                            id='filter-mobile-color-1'
+                            name='color[]'
+                            value='beige'
+                            type='checkbox'
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                          />
+                          <label
+                            htmlFor='filter-mobile-color-1'
+                            className='ml-3 min-w-0 flex-1 text-gray-500'
+                          >
+                            Beige
+                          </label>
+                        </div>
+                        <div className='flex items-center'>
+                          <input
+                            id='filter-mobile-color-2'
+                            name='color[]'
+                            value='blue'
+                            type='checkbox'
+                            checked
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                          />
+                          <label
+                            htmlFor='filter-mobile-color-2'
+                            className='ml-3 min-w-0 flex-1 text-gray-500'
+                          >
+                            Blue
+                          </label>
+                        </div>
+                        <div className='flex items-center'>
+                          <input
+                            id='filter-mobile-color-3'
+                            name='color[]'
+                            value='brown'
+                            type='checkbox'
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                          />
+                          <label
+                            htmlFor='filter-mobile-color-3'
+                            className='ml-3 min-w-0 flex-1 text-gray-500'
+                          >
+                            Brown
+                          </label>
+                        </div>
+                        <div className='flex items-center'>
+                          <input
+                            id='filter-mobile-color-4'
+                            name='color[]'
+                            value='green'
+                            type='checkbox'
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                          />
+                          <label
+                            htmlFor='filter-mobile-color-4'
+                            className='ml-3 min-w-0 flex-1 text-gray-500'
+                          >
+                            Green
+                          </label>
+                        </div>
+                        <div className='flex items-center'>
+                          <input
+                            id='filter-mobile-color-5'
+                            name='color[]'
+                            value='purple'
+                            type='checkbox'
+                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                          />
+                          <label
+                            htmlFor='filter-mobile-color-5'
+                            className='ml-3 min-w-0 flex-1 text-gray-500'
+                          >
+                            Purple
+                          </label>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  )}
                 </div>
 
                 <div className='border-t border-gray-200 px-4 py-6'>
