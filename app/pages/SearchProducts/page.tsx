fda370d1a3bae0387e2d486d0487233226ba1b23
hamzatalha77 <hamza_ta7@hotmail.com>
2024-10-08 12:07:03 +0100

    commit

--- app/pages/SearchProducts/page.tsx
+++ app/pages/SearchProducts/page.tsx
@@ -500,7 +500,7 @@ const SearchProducts = () => {
           </div>
         </div>
 
-        <main className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+        <main className='mx-auto max-w-[95rem] px-4 sm:px-6 lg:px-8'>
           <div className='flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24'>
             <h1 className='text-4xl font-bold tracking-tight text-gray-900'>
               New Arrivals
@@ -1003,7 +1003,312 @@ const SearchProducts = () => {
                 </div>
               </form>
 
-              <div className='lg:col-span-3'></div>
+              <div className='lg:col-span-3'>
+                <div className='bg-white py-6 sm:py-8 lg:py-12'>
+                  <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+                    <div className='grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-3'>
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Austin Wade'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+
+                          <div className='absolute bottom-2 left-0 flex gap-2'>
+                            <span className='rounded-r-lg bg-red-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-wider text-white'>
+                              -50%
+                            </span>
+                            <span className='rounded-lg bg-white px-3 py-1.5 text-sm font-bold uppercase tracking-wider text-gray-800'>
+                              New
+                            </span>
+                          </div>
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Fancy Outfit
+                            </a>
+                            <span className='text-gray-500'>
+                              by Fancy Brand
+                            </span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $19.99
+                            </span>
+                            <span className='text-sm text-red-500 line-through'>
+                              $39.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1523359346063-d879354c0ea5?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Nick Karvounis'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Cool Outfit
+                            </a>
+                            <span className='text-gray-500'>by Cool Brand</span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $29.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1523359346063-d879354c0ea5?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Nick Karvounis'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Cool Outfit
+                            </a>
+                            <span className='text-gray-500'>by Cool Brand</span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $29.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1523359346063-d879354c0ea5?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Nick Karvounis'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Cool Outfit
+                            </a>
+                            <span className='text-gray-500'>by Cool Brand</span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $29.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1523359346063-d879354c0ea5?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Nick Karvounis'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Cool Outfit
+                            </a>
+                            <span className='text-gray-500'>by Cool Brand</span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $29.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1523359346063-d879354c0ea5?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Nick Karvounis'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Cool Outfit
+                            </a>
+                            <span className='text-gray-500'>by Cool Brand</span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $29.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1523359346063-d879354c0ea5?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Nick Karvounis'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Cool Outfit
+                            </a>
+                            <span className='text-gray-500'>by Cool Brand</span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $29.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1548286978-f218023f8d18?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Austin Wade'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Nice Outfit
+                            </a>
+                            <span className='text-gray-500'>by Nice Brand</span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $35.00
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div>
+                        <a
+                          href='#'
+                          className='group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3'
+                        >
+                          <img
+                            src='https://images.unsplash.com/photo-1566207274740-0f8cf6b7d5a5?auto=format&q=75&fit=crop&crop=top&w=600&h=700'
+                            loading='lazy'
+                            alt='Photo by Vladimir Fedotov'
+                            className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                          />
+                        </a>
+
+                        <div className='flex items-start justify-between gap-2 px-2'>
+                          <div className='flex flex-col'>
+                            <a
+                              href='#'
+                              className='text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl'
+                            >
+                              Lavish Outfit
+                            </a>
+                            <span className='text-gray-500'>
+                              by Lavish Brand
+                            </span>
+                          </div>
+
+                          <div className='flex flex-col items-end'>
+                            <span className='font-bold text-gray-600 lg:text-lg'>
+                              $49.99
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </section>
         </main>
