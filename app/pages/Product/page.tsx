400c32473768ec87a5126f7fb00c47ba1f8d7bf9
hamzatalha77 <hamza_ta7@hotmail.com>
2024-10-14 20:41:58 +0100

    commit

--- app/pages/Product/page.tsx
+++ app/pages/Product/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
-import 'swiper/swiper-bundle.min.css'
+// import 'swiper/swiper-bundle.min.css'
 
 const Product = () => {
   return (
