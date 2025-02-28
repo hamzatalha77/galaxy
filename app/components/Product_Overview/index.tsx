bb8272b43dd21a837f1ece32817756905994416b
hamzatalha77 <hamza_ta7@hotmail.com>
2025-02-28 10:55:46 +0000

    commit

---
+++ app/components/Product_Overview/index.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Thumbs } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/thumbs'
+
+const images = [
+  'https://pagedone.io/asset/uploads/1700472379.png',
+  'https://pagedone.io/asset/uploads/1711622397.png',
+  'https://pagedone.io/asset/uploads/1711622408.png',
+  'https://pagedone.io/asset/uploads/1711622419.png',
+  'https://pagedone.io/asset/uploads/1711622437.png'
+]
+const Product_Overview = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+  return <div></div>
+}
+
+export default Product_Overview
